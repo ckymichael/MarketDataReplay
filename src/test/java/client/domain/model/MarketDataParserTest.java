3b76aa615ca78555cb4ba6196e9b282193fe14f9
ckymichael <michaelcheung1998@gmail.com>
2022-11-10 09:59:50 +0800

    added test case for parsing base price

--- src/test/java/client/domain/model/MarketDataParserTest.java
+++ src/test/java/client/domain/model/MarketDataParserTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MarketDataParserTest {
     String message = "44570:T:9104,09:01:11.254769000,10930.00,354600,10890.6318,0";
+    String message1 = "1:B:1301,3480.00";
     Trade expectTrade = Trade.builder()
             .sequenceNumber(44570)
             .code("9104")
@@ -20,8 +21,14 @@ class MarketDataParserTest {
             .isClose(false)
             .build();
 
+    BasePrice expectedPrice = BasePrice.builder()
+            .sequenceNumber(1)
+            .code("1301")
+            .basePrice(3480d)
+            .build();
+
     @Test
-    void apply() {
+    void parseTrade() {
         MarketData marketData = new MarketDataParser().apply(message);
         assertEquals(marketData.getClass(), Trade.class);
         Trade trade = (Trade) marketData;
@@ -33,5 +40,16 @@ class MarketDataParserTest {
         assertEquals(expectTrade.getVwap(), trade.getVwap());
         assertEquals(expectTrade.isClose(), trade.isClose());
         assertEquals(expectTrade.getTurnover(), trade.getTurnover());
+    }
+
+    @Test
+    void parseBasePrice() {
+        MarketData marketData = new MarketDataParser().apply(message1);
+        assertEquals(marketData.getClass(), BasePrice.class);
+
+        BasePrice price = (BasePrice) marketData;
+        assertEquals(price.getSequenceNumber(), expectedPrice.getSequenceNumber());
+        assertEquals(price.getCode(), expectedPrice.getCode());
+        assertEquals(price.getBasePrice(), expectedPrice.getBasePrice());
     }
 }
